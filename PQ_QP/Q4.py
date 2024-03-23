bdc694b7928e7860fdcfa23a10e1c7de77e714cd
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-03-23 00:31:58 -0800

    .

---
+++ PQ_QP/Q4.py
@@ -0,0 +1,12 @@
+with open('code.txt','w') as fo:
+    fo.write('PYTHON PROGRAMMING')
+with open('code.txt','r') as fr:
+    print(fr.read())
+          
+
+# fo = open("code.txt",'w')
+# fo.write('PROGRAMMING IN PYTHON')
+# fo.close()
+
+# fr = open('code.txt','r')
+# print(fr.read())
